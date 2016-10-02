03d7c7d3e79cfa23ff39e85cd2d424c579884986
sebastian Higuita castañeda <SHACKOX@SHACKOX-2.local>
2016-10-01 22:47:31 -0500

    fix register token to server to identify phone in the cloud

--- app/src/main/java/sebastianhiguita/shackox/me/com/aeropushnotification/notifications/PushNotificationsActivity.java
+++ app/src/main/java/sebastianhiguita/shackox/me/com/aeropushnotification/notifications/PushNotificationsActivity.java
@@ -4,11 +4,21 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 
+import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.messaging.FirebaseMessaging;
 
+import java.io.IOException;
+
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
 import sebastianhiguita.shackox.me.com.aeropushnotification.R;
 
 public class PushNotificationsActivity extends AppCompatActivity {
+
+    String Token;
+    boolean thread_running = true;
 
     private static final String TAG = PushNotificationsActivity.class.getSimpleName();
 
@@ -23,6 +33,30 @@ public class PushNotificationsActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         setTitle(getString(R.string.title_activity_notifications));
 
+        Thread t = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (thread_running) {
+                    Token = FirebaseInstanceId.getInstance().getToken();
+                    if (Token != null) {
+                        System.out.println("Device Token is " + Token);
+                        registerToken(Token);
+
+                        thread_running = false;
+                    } else {
+                        System.out.println(" - Token not loaded -");
+                    }
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+
+        t.start();
+
         mNotificationsFragment = (PushNotificationsFragment) getSupportFragmentManager().findFragmentById(R.id.notifications_container);
 
         if (mNotificationsFragment == null) {
@@ -34,6 +68,26 @@ public class PushNotificationsActivity extends AppCompatActivity {
         }
 
         mNotificationsPresenter = new PushNotificationsPresenter(mNotificationsFragment, FirebaseMessaging.getInstance());
+    }
+
+    private void registerToken(String token) {
+
+        OkHttpClient client = new OkHttpClient();
+        RequestBody body = new FormBody.Builder()
+                .add("Token",token)
+                .build();
+
+        Request request = new Request.Builder()
+                .url("https://cloudapp-manzza.rhcloud.com/Push/DeviceRegister.php") // cloud development
+                //.url("http://175.35.0.15/fcm/register.php") //local development
+                .post(body)
+                .build();
+
+        try {
+            client.newCall(request).execute();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
